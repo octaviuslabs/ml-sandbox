c83d5b6ec4e296e1479a9947b988241025fa87f4
Jimi Smoot <jsfour@gmail.com>
2015-06-19 16:47:06 -0700

    Added another command

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,13 @@
 apt-get update
 apt-get -y install pandoc
 apt-get -y install libzmq-dev
+apt-get -y install python-zmq
 apt-get -y install python-matplotlib
 apt-get -y install python-pip
-pip install jinja2 sphinx pyzmq pygments tornado mock "ipython[notebook]"
+pip install jinja2
+pip install sphinx
+pip install pyzmq
+pip install pygments
+pip install tornado
+pip install mock
+pip install "ipython[notebook]"
